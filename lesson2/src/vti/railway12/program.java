880d796bb81f697fc14091246d7d3c9b2fc3b904
Luc-BuiTien <Tienluc220296@gmail.com>
2021-06-06 10:18:10 +0700

    sua file program

--- lesson2/src/vti/railway12/program.java
+++ lesson2/src/vti/railway12/program.java
@@ -5,6 +5,8 @@ public class program {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stu
 		System.out.println("hello world");
+		System.out.println("day la github cua toi");
+		
 	}
 
 }
